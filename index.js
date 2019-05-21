029e16479942a89cb1e59c4412d17b6d6c56dd0d
Dylan Harris <dylanharris23@gmail.com>
2019-05-21 20:47:08 +0000

    Automatically backed up by Learn

---
+++ index.js
@@ -0,0 +1,17 @@
+function shout(String) {
+  return String.toUpperCase()
+}
+function whisper(String) {
+  return String.toLowerCase()
+}
+function logShout(String) {
+  console.log(String.toUpperCase())
+}
+function logWhisper(String) {
+  console.log(String.toLowerCase())
+}
+function sayHiToGrandma(String) {
+  return "I can't hear you!"
+}
+function sayHiToGrandma(String) {
+  return "YES INDEED!" if (String.toLowerCase())
